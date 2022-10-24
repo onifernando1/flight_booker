ae1e3f03fbf3ed8130407de1d49f9bb20c32ca82
onifernando1 <onifernando1@gmail.com>
2022-10-24 16:19:25 +0100

    Create options on get form flights index

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,7 +2,10 @@ class FlightsController < ApplicationController
 
     def index
         @flights = Flight.all  
+        @airport_options = Airport.all.map {|f|[f.name, f.id]}
+        @departure_date_options = Flight.all.map {|f| [f.departure_time.strftime("%d/%m/%Y"), f.id]}
+        @arrival_date_options = Flight.all.map {|f| [f.arrival_time.strftime("%d/%m/%Y"), f.id]}
     end 
 
-    
+
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,6 +3,11 @@ class Flight < ApplicationRecord
     belongs_to :departure_airport, foreign_key:"departure_airport", class_name: 'Airport'
     belongs_to :arrival_airport, foreign_key:"arrival_airport", class_name: 'Airport'
     
+    def departure_date_formatted
+        departure_time.strftime("%d/%m/%Y")
+    end
 
-
+    def arrival_date_formatted
+        arrival_time.strftime("%d/%m/%Y")
+    end
 end
