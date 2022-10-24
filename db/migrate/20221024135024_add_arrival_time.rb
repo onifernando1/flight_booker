4dadfc2855250d24e6abfc46f989539baee2dce2
onifernando1 <onifernando1@gmail.com>
2022-10-24 14:58:01 +0100

    Add arrival time

---
+++ db/migrate/20221024135024_add_arrival_time.rb
@@ -0,0 +1,5 @@
+class AddArrivalTime < ActiveRecord::Migration[7.0]
+  def change
+    add_column :flights, :arrival_time, :datetime
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Airport.delete_all
+lgw = Airport.create(name:"London Gatwick", code:"LGW")
+mad = Airport.create(name:"Madrid", code:"MAD")
+mxp = Airport.create(name:"Milan Malpensa", code:"MXP")
+del  = Airport.create(name:"New Delhi", code:"DEL")
+sjo = Airport.create(name:"San Jose", code:"SJO")
+nrt = Airport.create(name:"Tokyo Narita", code:"NRT")
+
+
+Flight.create(departure_airport: lgw, departure_time, arrival_airport:, arrival_time, departure)
+
