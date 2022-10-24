88c7fae0f4c2ad9f6c5301875983c38259253ca4
onifernando1 <onifernando1@gmail.com>
2022-10-24 15:16:21 +0100

    Begin seeding flights and airports

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,18 @@ del  = Airport.create(name:"New Delhi", code:"DEL")
 sjo = Airport.create(name:"San Jose", code:"SJO")
 nrt = Airport.create(name:"Tokyo Narita", code:"NRT")
 
+Flight.delete_all
+Flight.create(departure_airport:lgw, departure_time:"2030-12-25 10:00:00", duration: 150, arrival_airport:mad, arrival_time:"2030-12-25 13:30:00")
+Flight.create(departure_airport:mad, departure_time:"2030-12-26 15:00:00", duration:145, arrival_airport:lgw, arrival_time:"2030-12-26 16:25:00")
+Flight.create(departure_airport:lgw, departure_time:"2030-12-27 20:40:00", duration: 139, arrival_airport:mad, arrival_time:"2030-12-27 23:59:00")
+Flight.create(departure_airport:mad, departure_time:"2030-12-28 18:40:00", duration: 140, arrival_airport:lgw, arrival_time:"2030-12-28 20:00:00")
 
-Flight.create(departure_airport: lgw, departure_time, arrival_airport:, arrival_time, departure)
+Flight.create(departure_airport:mad, departure_time:"2030-12-25 09:05:00", duration:130, arrival_airport:mxp, arrival_time:"2030-12-25 11:15:00")
+Flight.create(departure_airport:mxp, departure_time:"2030-12-26 06:10:00", duration:140, arrival_airport:mad, arrival_time:"2030-12-26 08:30:00")
+Flight.create(departure_airport:mad, departure_time:, duration:, arrival_airport:mxp, arrival_time:)
+Flight.create(departure_airport:mxp, departure_time:, duration:, arrival_airport:mad, arrival_time:)
+
+
+Flight.create(departure_airport:, departure_time:, duration:, arrival_airport:, arrival_time:)
+Flight.create(departure_airport:, departure_time:, duration:, arrival_airport:, arrival_time:)
 
