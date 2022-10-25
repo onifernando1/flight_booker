875c15379ed4f904c330d7298e19ae22d45669c3
onifernando1 <onifernando1@gmail.com>
2022-10-25 22:59:49 +0100

    Change flight and airport models to match id

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,7 +1,7 @@
 class Flight < ApplicationRecord
 
-    belongs_to :departure_airport, foreign_key:"departure_airport", class_name: 'Airport'
-    belongs_to :arrival_airport, foreign_key:"arrival_airport", class_name: 'Airport'
+    belongs_to :departure_airport, foreign_key:"departure_airport_id", class_name: 'Airport'
+    belongs_to :arrival_airport, foreign_key:"arrival_airport_id", class_name: 'Airport'
     
     # def departure_date_uniq
     #     departure_date.uniq
