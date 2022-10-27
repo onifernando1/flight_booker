6ca5338ee3072283dcd2410c080bc6554ae5a5cd
onifernando1 <onifernando1@gmail.com>
2022-10-27 17:26:02 +0100

    new bookings controller, edit flights/index form to create booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,12 @@
 class BookingsController < ApplicationController
+
+    def new
+        @booking = Booking.new() 
+        @flight = Flight.new(flight_params) #necessary?
+    end 
+
+    def flight_params #necessary?
+        params.permit(:departure_airport_id, :arrival_airport_id, :departure_date)
+    end 
+
 end
