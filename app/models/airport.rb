6178e41670eef20337ebb9f1fc3aaf45fb9dc95c
onifernando1 <onifernando1@gmail.com>
2022-10-27 17:19:21 +0100

    Bookings controller made

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -2,5 +2,6 @@ class Airport < ApplicationRecord
 
     has_many :departing_flights, foreign_key: "departure_airport_id", class_name: 'Flight'
     has_many :arriving_flights, foreign_key: "arrival_airport_id", class_name: 'Flight'
+    belongs_to_many :bookings
 
 end
