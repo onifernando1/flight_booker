1fe5950fbda2bf24edd926d709042dcee24b1354
onifernando1 <onifernando1@gmail.com>
2022-10-28 16:26:02 +0100

    Flights index second form getting from bookings/new, bookings new controller method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,8 @@
 class BookingsController < ApplicationController
 
     def new
-        @booking = Booking.new() 
-        @flight = Flight.new(flight_params) #necessary?
+        @booking = Booking.new(params[:flight_id]) 
     end 
 
-    def flight_params #necessary?
-        params.permit(:departure_airport_id, :arrival_airport_id, :departure_date)
-    end 
-
+ 
 end
