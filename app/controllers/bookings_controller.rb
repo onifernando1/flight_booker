6e49c504fc09ff392e6b7c2cc0324c9689b6ec0f
onifernando1 <onifernando1@gmail.com>
2022-10-29 22:44:39 +0100

    Add booking foreign key to passengers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,8 +6,41 @@ class BookingsController < ApplicationController
 
     end 
 
+    def create 
+        @booking = Booking.new(booking_params)
+        if @booking.save
+            redirect_to @booking
+        else 
+            render :new, status: :unprocessable_entity
+        end 
+
+        @booking.passenger 
+
+        # @passenger = Passenger.new(passenger_params)
+        # if @passenger.save 
+        #     redirect_to @booking 
+        # else 
+        #     render :new, status: :unprocessable_entity
+        # end 
+    end 
+
+
+
     def flight_params 
         params.permit(:flight)
+    end 
+
+    def create_passengers
+        @number_of_passengers = params[:number_of_passengers]
+        # @number_of_passengers.to_i.times do {@booking.build_passenger} end 
+    end 
+
+    def booking_params
+        params.require(:booking).permit(:flight_id)
+    end 
+
+    def passenger_params
+        params.require(:passenger).permit(:name, :email)
     end 
 
  
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_27_152818) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_29_214345) do
   create_table "airports", force: :cascade do |t|
     t.string "name"
     t.string "code"
@@ -42,6 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_152818) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "booking_id"
   end
 
 end
