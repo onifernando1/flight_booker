d94d1c61a0f2730cf857312a36c41c6ea0c154a1
onifernando1 <onifernando1@gmail.com>
2022-10-29 17:11:59 +0100

    Fix id on flightsindex booking form, select flight via params[flight] in booking contorller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,17 @@
 class BookingsController < ApplicationController
 
     def new
-        @booking = Booking.new(params[:flight_id]) 
+        @booking = Booking.new() 
+        @selected_flight = Flight.find(params[:flight])
     end 
+
+    def bookings_params 
+        params.permit(:flight_id)
+    end 
+
+    # def flight_params 
+    #     params.permit(:departure_airport_id, :arrival_airport_id, :departure_date)
+    # end 
 
  
 end
