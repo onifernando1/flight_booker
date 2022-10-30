af9b7a248fe5c0ebc85d9b348f3f81cf34661a1e
onifernando1 <onifernando1@gmail.com>
2022-10-30 23:06:58 +0000

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,23 +8,22 @@
 Airport.delete_all
 lgw = Airport.create(id:1,name:"London Gatwick", code:"LGW")
 mad = Airport.create(id:2,name:"Madrid", code:"MAD")
-mxp = Airport.create(id:3,name:"Milan Malpensa", code:"MXP")
-del  = Airport.create(id:4,name:"New Delhi", code:"DEL")
+# mxp = Airport.create(id:3,name:"Milan Malpensa", code:"MXP")
+# del  = Airport.create(id:4,name:"New Delhi", code:"DEL")
 
 Flight.delete_all
 Flight.create(departure_airport_id:1, departure_date:"2030-12-25", departure_time:"10:00:00", duration: 150, arrival_airport_id:2, arrival_date:"2030-12-25",arrival_time: "13:30:00",price:120)
-Flight.create(departure_airport_id:mad.id, departure_date:"2030-12-26", departure_time:"15:00:00", duration:145, arrival_airport_id:lgw.id, arrival_date:"2030-12-26",arrival_time: "16:25:00",price:160)
+Flight.create(departure_airport_id:1, departure_date:"2030-12-26", departure_time:"10:00:00", duration: 150, arrival_airport_id:2, arrival_date:"2030-12-26",arrival_time: "13:30:00",price:120)
 Flight.create(departure_airport_id:lgw.id, departure_date:"2030-12-25", departure_time:"13:00:00", duration: 150, arrival_airport_id:mad.id, arrival_date:"2030-12-25",arrival_time: "16:30:00",price:120)
+Flight.create(departure_airport_id:lgw.id, departure_date:"2030-12-26", departure_time:"13:00:00", duration: 150, arrival_airport_id:mad.id, arrival_date:"2030-12-26",arrival_time: "16:30:00",price:120)
+Flight.create(departure_airport_id:lgw.id, departure_date:"2030-12-26", departure_time:"20:40:00", duration: 139, arrival_airport_id:mad.id, arrival_date:"2030-12-26",arrival_time: "23:59:00",price:175)
+Flight.create(departure_airport_id:lgw.id, departure_date:"2030-12-25", departure_time:"20:40:00", duration: 139, arrival_airport_id:mad.id, arrival_date:"2030-12-25",arrival_time: "23:59:00",price:175)
+
+Flight.create(departure_airport_id:mad.id, departure_date:"2030-12-26", departure_time:"15:00:00", duration:145, arrival_airport_id:lgw.id, arrival_date:"2030-12-26",arrival_time: "16:25:00",price:160)
+Flight.create(departure_airport_id:mad.id, departure_date:"2030-12-25", departure_time:"15:00:00", duration:145, arrival_airport_id:lgw.id, arrival_date:"2030-12-25",arrival_time: "16:25:00",price:160)
 Flight.create(departure_airport_id:mad.id, departure_date:"2030-12-26", departure_time:"18:00:00", duration:145, arrival_airport_id:lgw.id, arrival_date:"2030-12-26",arrival_time: "19:25:00",price:160)
-Flight.create(departure_airport_id:lgw.id, departure_date:"2030-12-27", departure_time:"20:40:00", duration: 139, arrival_airport_id:mad.id, arrival_date:"2030-12-27",arrival_time: "23:59:00",price:175)
-Flight.create(departure_airport_id:mad.id, departure_date:"2030-12-28", departure_time:"18:40:00", duration: 140, arrival_airport_id:lgw.id, arrival_date:"2030-12-28",arrival_time: "20:00:00",price:210)
-Flight.create(departure_airport_id:mad.id, departure_date:"2030-12-25", departure_time:"09:05:00", duration:130, arrival_airport_id:mxp.id, arrival_date:"2030-12-25",arrival_time: "11:15:00",price:85)
-Flight.create(departure_airport_id:mxp.id, departure_date:"2030-12-26", departure_time:"06:10:00", duration:140, arrival_airport_id:mad.id, arrival_date:"2030-12-26",arrival_time: "08:30:00",price:135)
-Flight.create(departure_airport_id:mad.id, departure_date:"2030-12-27", departure_time:"17:25:00", duration:130, arrival_airport_id:mxp.id, arrival_date:"2030-12-27",arrival_time: "19:35:00",price:100)
-Flight.create(departure_airport_id:mxp.id, departure_date:"2030-12-28", departure_time:"20:20:00", duration:145, arrival_airport_id:mad.id, arrival_date:"2030-12-28",arrival_time: "22:45:00",price:80)
-Flight.create(departure_airport_id:lgw.id, departure_date:"2030-12-25", departure_time:"18:15:00", duration:520, arrival_airport_id:del.id, arrival_date:"2030-12-26",arrival_time: "07:25:00",price:500)
-Flight.create(departure_airport_id:del.id, departure_date:"2030-12-27", departure_time:"09:25:00", duration:560, arrival_airport_id:lgw.id, arrival_date:"2030-12-27",arrival_time: "14:15:00",price:895)
-Flight.create(departure_airport_id:lgw.id, departure_date:"2030-12-28", departure_time:"18:15:00", duration:520, arrival_airport_id:del.id, arrival_date:"2030-12-29",arrival_time: "07:25:00",price:560)
-Flight.create(departure_airport_id:del.id, departure_date:"2030-12-30", departure_time:"02:40:00", duration:560, arrival_airport_id:lgw.id, arrival_date:"2030-12-30",arrival_time: "07:30:00",price:890)
+Flight.create(departure_airport_id:mad.id, departure_date:"2030-12-25", departure_time:"18:00:00", duration:145, arrival_airport_id:lgw.id, arrival_date:"2030-12-25",arrival_time: "19:25:00",price:160)
+Flight.create(departure_airport_id:mad.id, departure_date:"2030-12-25", departure_time:"18:40:00", duration: 140, arrival_airport_id:lgw.id, arrival_date:"2030-12-25",arrival_time: "20:00:00",price:210)
+Flight.create(departure_airport_id:mad.id, departure_date:"2030-12-26", departure_time:"18:40:00", duration: 140, arrival_airport_id:lgw.id, arrival_date:"2030-12-26",arrival_time: "20:00:00",price:210)
 
 # fix duplicates
