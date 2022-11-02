57f26848affda6a19dfaff5e555dffed383b2efe
onifernando1 <onifernando1@gmail.com>
2022-11-02 00:49:02 +0000

    welcome email func in passengermailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,8 @@
 class PassengerMailer < ApplicationMailer
+    default from: 'notifications@example.com'
+
+    def welcome_email 
+        @passenger = params[:passenger]
+        mail(to: @passenger.email, subject:"Welcome!")
+    end
 end
